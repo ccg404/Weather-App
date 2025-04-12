81982521b39db5f05214d452306edec04cbff725
Christian Carlo <galarritachristiancarlo3@gmail.com>
2025-04-12 21:28:40 +0800

    Implement geolocation fetching and error handling for weather data retrieval

--- script.js
+++ script.js
@@ -12,4 +12,34 @@ function fetchWeather(){
         </div>`;
         return;
     }
+
+    async function getLonAndLat() {
+        const countryCode = 1;
+        const geocodeURL = `https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}`;
+
+        const response = await fetch(geocodeURL);
+        if (!response.ok) {
+            console.log("Bad response", response.status);
+            return;
+        }
+
+        const data = await response.json();
+        if (data.length === 0) {
+            console.log("Something went wrong here");
+            weatherDataSection.innerHTML = `
+            <div>
+                <h2>Invalid Input: "${searchInput}"</h2>
+                <p>Please try again with a valid <u>city name</u>.</p>
+            </div>
+            `;
+            return;
+        }else{
+            return data[0];
+        }
+    }
+
+
+    async function getWeatherData(lot, lat) {
+        
+    }
 }
