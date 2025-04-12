6ee3d53bae663ad6bece1a7dd5747d7a4b167dfa
Christian Carlo <galarritachristiancarlo3@gmail.com>
2025-04-12 21:11:54 +0800

    Add input validation for weather search functionality

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ function fetchWeather(){
     const weatherDataSection = document.getElementById("weather-data");
     weatherDataSection.style.display = "block";
     const apiKey = "API KEY HERE";
+
+    if(searchInput === ""){
+        weatherDataSection.innerHTML = `
+        <div>
+            <h2>Empty Input!</h2>
+            <p>Please try again with a valid <u>city name</u>.</p>
+        </div>`;
+        return;
+    }
 }
